b18ee166b8466e6a4543957507810d049abb3ee1
王振 <wangzh@firstshare.cn>
2019-12-27 23:07:38 +0800

    import sys.path

--- CItest/testCase/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/python
-#-*- coding: utf-8 -*-
-#@Time:2019/12/27 9:04 PM
-#@Author: wangzhen
-#@FileName:__init__.py
-#@SoftWare:PyCharm
--- CiTest/__init__.py
+++ CiTest/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 #-*- coding: utf-8 -*-
-#@Time:2019/12/27 9:03 PM
+#@Time:2019/12/27 10:08 PM
 #@Author: wangzhen
 #@FileName:__init__.py
 #@SoftWare:PyCharm
